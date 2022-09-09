bb2e2d591eee6ec9c5f8ba5a06dd990374f3e125
Soraiya1 <rayathobani@gmail.com>
2022-09-09 16:57:54 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i;
+int ch;
+
+ch = 122;
+i = 26;
+while (i > 0)
+{
+putchar(ch);
+ch--;
+i--;
+}
+putchar(10);
+return (0);
+}
